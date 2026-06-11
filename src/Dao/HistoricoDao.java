69eeb0c0f86df8dd0172c1ad2dafed198dd5e17a
Eliel_Guedes.07 <elielguedes817@gmail.com>
2026-06-11 17:37:09 -0300

    add Dao historico

--- src/Dao/HistoricoDao.java
+++ src/Dao/HistoricoDao.java
@@ -38,4 +38,39 @@ public class HistoricoDao {
         return h;
     }
 
+    public void UpdateHistorico(Historico h) throws SQLException {
+        String sql = "update historico set nota = ? , frequencia = ? , matricula_id = ? where id_historico = ? ";
+        try(PreparedStatement ps = c.prepareStatement(sql)){
+            ps.setFloat(1 , h.getNota());
+            ps.setInt(2 , h.getFrequencia());
+            ps.setInt(3 , h.getMatricula());
+            ps.executeUpdate();
+        }
+    }
+
+    public void DeleteHistorico(Historico h) throws SQLException {
+        String sql = "DELETE FROM historico WHERE id_historico = ? ";
+        try(PreparedStatement ps = c.prepareStatement(sql)){
+            ps.setInt(1 , h.getId());
+            ps.executeUpdate();
+        }
+    }
+
+    public Historico Busca_id(int id) throws SQLException {
+        String sql = "SELECT id_historico,nota,frequencia,matricula_id FROM historico WHERE id_historico = ?";
+        try(PreparedStatement ps = c.prepareStatement(sql)){
+            ps.setInt(1 , id);
+            try(ResultSet rs = ps.executeQuery()){
+                return new Historico(
+                        rs.getInt("id_historico"),
+                        rs.getFloat("nota"),
+                        rs.getInt("frequencia"),
+                        rs.getInt("matricula_id")
+                );
+
+            }
+        }
+
+    }
+
 }
